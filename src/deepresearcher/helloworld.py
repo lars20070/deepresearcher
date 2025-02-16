ac473a06c4e29be70d46bb4b9931a25bf3bc0ce9
lars20070 <l.nilse@dunelm.org.uk>
2025-02-16 15:48:41 +0100

    HelloWorld class, switch off tests

---
+++ src/deepresearcher/helloworld.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from deepresearcher import logger
+
+
+class HelloWorld:
+    """
+    HelloWorld class
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize HelloWorld
+        """
+        logger.info("Initializing HelloWorld.")
+        pass
